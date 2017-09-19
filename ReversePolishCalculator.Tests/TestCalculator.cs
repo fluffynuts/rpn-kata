72e12ac94d4b0bcf266b12cfdea7f922fdfd8d7d
Davyd McColl <davydm@gmail.com>
2017-09-19 13:54:15 +0200

    first test -- fails

--- ReversePolishCalculator.Tests/TestCalculator.cs
+++ ReversePolishCalculator.Tests/TestCalculator.cs
@@ -9,10 +9,51 @@ namespace ReversePolishCalculator.Tests
     [TestFixture]
     public class TestCalculator
     {
+        [TestFixture]
+        public class InvalidInputShouldThrowArgumentExceptions
+        {
+            [Test]
+            public void GivenEmptyString_ShouldThrow_ArgumentException()
+            {
+                // Arrange
+                var input = "";
+                var sut = Create();
+                // Pre-Assert
+                // Act
+                Expect(() => sut.Calculate(input))
+                    .To.Throw<ArgumentException>();
+                // Assert
+            }
+
+            [Test]
+            public void GivenOnlyOneInteger()
+            {
+                // Arrange
+                var input = "1";
+                var sut = Create();
+                // Pre-Assert
+                // Act
+                Expect(() => sut.Calculate(input))
+                    .To.Throw<ArgumentException>();
+                // Assert
+            }
+
+            [Test]
+            public void GivenNull()
+            {
+                // Arrange
+                var sut = Create();
+                // Pre-Assert
+                // Act
+                Expect(() => sut.Calculate(null))
+                    .To.Throw<ArgumentException>();
+                // Assert
+            }
+        }
 
         private static ICalculator Create()
         {
-            throw new NotImplementedException();
+            return new Calculator();
         }
     }
 }
--- ReversePolishCalculator/Calculator.cs
+++ ReversePolishCalculator/Calculator.cs
@@ -7,4 +7,11 @@ namespace ReversePolishCalculator
         int Calculate(string input);
     }
 
+    public class Calculator: ICalculator
+    {
+        public int Calculate(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
