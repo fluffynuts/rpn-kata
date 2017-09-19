ada26cb8a0f48235bc44e72d73ea1816c8fba94d
Davyd McColl <davydm@gmail.com>
2017-09-19 13:53:29 +0200

    feat: start kata

---
+++ ReversePolishCalculator.Tests/TestCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit;
+using NExpect;
+using NUnit.Framework;
+using static NExpect.Expectations;
+
+namespace ReversePolishCalculator.Tests
+{
+    [TestFixture]
+    public class TestCalculator
+    {
+
+        private static ICalculator Create()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ReversePolishCalculator/Calculator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ReversePolishCalculator
+{
+    public interface ICalculator
+    {
+        int Calculate(string input);
+    }
+
+}
