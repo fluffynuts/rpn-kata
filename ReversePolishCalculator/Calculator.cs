e12aee06ad2795b0cd67ad36f0996f953ffdbef8
Davyd McColl <davydm@gmail.com>
2017-09-19 14:02:45 +0200

    refactor: make calculator look a little more fluent

--- ReversePolishCalculator/Calculator.cs
+++ ReversePolishCalculator/Calculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ReversePolishCalculator
@@ -8,23 +9,46 @@ namespace ReversePolishCalculator
         int Calculate(string input);
     }
 
-    public class Calculator: ICalculator
+    public class Calculator : ICalculator
     {
         public int Calculate(string input)
         {
-            if (string.IsNullOrEmpty(input))
-            {
-                throw new ArgumentException();
-            }
             var parts = input
-                           .Split(new[] { " " }, StringSplitOptions.None)
-                           .Select(int.Parse)
-                           .ToArray();
-            if (parts.Length != 2)
-            {
-                throw new ArgumentException();
-            }
+                .EmptyIfNull()
+                .Split(new[] {" "}, StringSplitOptions.None)
+                .ToInts();
             return parts.Sum();
+        }
+    }
+
+    internal static class Extensions
+    {
+        internal static string EmptyIfNull(this string str)
+        {
+            return str ?? "";
+        }
+
+        internal static int[] ToInts(this string[] strings)
+        {
+            return strings
+                .TryConvertToInts()
+                .ValidateHaveTwoInts();
+        }
+
+        internal static IEnumerable<int> TryConvertToInts(this string[] strings)
+        {
+            return strings.Select(s =>
+                int.TryParse(s, out var result)
+                    ? result
+                    : throw new ArgumentException());
+        }
+
+        internal static int[] ValidateHaveTwoInts(this IEnumerable<int> ints)
+        {
+            var result = ints.ToArray();
+            return result.Length == 2
+                ? result
+                : throw new ArgumentException();
         }
     }
 }
